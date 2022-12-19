891f0f1f3ba4dda6abc011bc65f76cbef95c5334
German Enik <germanenik@gmail.com>
2022-12-19 00:40:31 -0800

    debugging svgs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './svgs/first.svg';
+import first from './svgs/first.svg';
+import second from './svgs/second.svg';
 import './App.css';
 import SplashScreen from './screens/SplashScreen';
 
@@ -6,10 +7,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div style={{display:"flex", flexDirection:"row"}}>
-          <img src={logo} className="App-logo" alt="logo" style={{opacity:0.5}} />
-          <img src={logo} className="App-logo" alt="logo" style={{opacity:0.5}} />
-        </div>
+        {/* <div style={{display:"flex", flexDirection:"row"}}> */}
+          <img src={first} className="App-logo" alt="logo" style={{opacity:0.5}} />
+        {/* <img src={second} className="App-logo" alt="logo" style={{opacity:0.5}} /> */}
+        {/* </div> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
