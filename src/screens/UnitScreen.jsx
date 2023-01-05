2f4f78fab436c04af4e489f126e9c53c0b19b71f
German Enik <germanenik@gmail.com>
2023-01-04 19:23:47 -0800

    adjusted styling

--- src/screens/UnitScreen.jsx
+++ src/screens/UnitScreen.jsx
@@ -120,20 +120,20 @@ const backgroundGlyphs = {
         {className: "background-glyph", src: glyph_4_p1, style: {position:"absolute", height:"80vh", bottom: "2vh", right:0, transform:"translateX(50%)", opacity: 0.3, zIndex: 0}},
     ],
     unit2: [ // abjads
-        {className: "background-glyph", src: glyph_1_p4, style: {position:"absolute", height:"85vh", bottom: "2vh", transform:"translateX(-62%)", opacity: 0.2, zIndex: 0}},
-        {className: "background-glyph", src: glyph_3_n1, style: {position:"absolute", height:"85vh", bottom: "2vh", right:0, transform:"translateX(25%)", opacity: 0.2, zIndex: 0}},
+        {className: "background-glyph", src: glyph_1_p4, style: {position:"absolute", height:"85vh", bottom: "2vh", transform:"translateX(-62%)", opacity: 0.5, zIndex: 0}},
+        {className: "background-glyph", src: glyph_3_n1, style: {position:"absolute", height:"85vh", bottom: "2vh", right:0, transform:"translateX(25%)", opacity: 0.5, zIndex: 0}},
     ],
     unit3: [ // abugidas
         {className: "background-glyph", src: glyph_1_n7, style: {position:"absolute", height:"80vh", bottom: "2vh", transform:"translateX(-55%)", opacity: 0.4, zIndex: 0}},
         {className: "background-glyph", src: glyph_1_n6, style: {position:"absolute", height:"80vh", bottom: "2vh", right:0, transform:"translateX(50%)", opacity: 0.4, zIndex: 0}},
     ],
     unit4: [ // syllabaries
-        {className: "background-glyph", src: glyph_2_n2, style: {position:"absolute", height:"80vh", bottom: "2vh", transform:"translateX(-50%)", opacity: 0.8, zIndex: 0}},
-        {className: "background-glyph", src: glyph_4_n5, style: {position:"absolute", height:"80vh", bottom: "2vh", right:0, transform:"translateX(50%)", opacity: 0.8, zIndex: 0}},
+        {className: "background-glyph", src: glyph_2_n2, style: {position:"absolute", height:"80vh", bottom: "2vh", transform:"translateX(-50%)", opacity: 0.6, zIndex: 0}},
+        {className: "background-glyph", src: glyph_4_n5, style: {position:"absolute", height:"80vh", bottom: "2vh", right:0, transform:"translateX(50%)", opacity: 0.6, zIndex: 0}},
     ],
     unit5: [ // alphasyllabaries
-        {className: "background-glyph", src: glyph_2_p4, style: {position:"absolute", height:"80vh", bottom: "2vh", transform:"translateX(-70%)", opacity: 0.8, zIndex: 0}},
-        {className: "background-glyph", src: glyph_4_0, style: {position:"absolute", height:"80vh", bottom: "2vh", right:0, transform:"translateX(70%)", opacity: 0.8, zIndex: 0}},
+        {className: "background-glyph", src: glyph_2_p4, style: {position:"absolute", height:"80vh", bottom: "2vh", transform:"translateX(-70%)", opacity: 0.35, zIndex: 0}},
+        {className: "background-glyph", src: glyph_4_0, style: {position:"absolute", height:"80vh", bottom: "2vh", right:0, transform:"translateX(70%)", opacity: 0.35, zIndex: 0}},
     ],
     unit6: [ // logosyllabaries
         {className: "background-glyph", src: glyph_1_p3, style: {position:"absolute", height:"80vh", bottom: "2vh", transform:"translateX(-60%)", opacity: 0.6, zIndex: 0}},
--- src/unitStyles.js
+++ src/unitStyles.js
@@ -8,30 +8,30 @@ const styles = {
     },
     unit2: {
         color1: "#798c5f",
-        color2: "#a2b18d", 
+        color2: "#D4D7CE", 
         color3: "#74875b",
         color4: "#314e00",
         textColor: "black"
     },
     unit3: {
         color1: "#edcb96",
-        color2: "#f3ddbc",
+        color2: "#EAE2D5",
         color3: "#e8bc79",
         color4: "#da991b",
         textColor: "black"
     },
     unit4: {
         color1: "#ffb9d6",
-        color2: '#FDEFF2',//"#F2E3EA",
-        color3: '#E4D2D8', //"#fe94c1",
-        color4: '#F6BFBC', //"#fd63a4",
+        color2: '#F3E5E4',//"#F2E3EA",
+        color3: '#D1C5C4', //"#fe94c1",
+        color4: '#EF9AC0', //"#fd63a4",
         textColor: "black"
     },
     unit5: {
-        color1: "#9bd2fd",
-        color2: '#F0EFF7', //"#d8edfe",
-        color3: '#8ABFE8', //"#8bcbfc",
-        color4: '#869FDE', // "#65b9fb",
+        color1: "#4E90ED",
+        color2: '#C8DBF5', //"#d8edfe",
+        color3: '#8CB7F3', //"#8bcbfc",
+        color4: '#4E90ED', // "#65b9fb",
         textColor: "black"
     },
     unit6: {
@@ -45,9 +45,15 @@ const styles = {
         color1: "#595959",
         color2: "#0d0d0d",
         color3: "#282828",
-        color4: "#353535",
+        color4: "#575757",
         textColor: "#f1f1f1"
     },
 }
+
+/*        color1: '#EF9AC0', // "#ffb9d6",
+        color2: '#DACAD1', // '#FDEFF2',//"#F2E3EA",
+        color3: '#DCA9BF', //'#E4D2D8', //"#fe94c1",
+        color4: '#EF9AC0', // '#F6BFBC', //"#fd63a4",
+ */
 
 export default styles;
