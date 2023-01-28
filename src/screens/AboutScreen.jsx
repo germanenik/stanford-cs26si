4c54668f713b9e306b52dc9d7b7db42d7748b824
German Enik <germanenik@gmail.com>
2023-01-28 02:59:39 -0800

    made about work for mobile

--- src/screens/AboutScreen.jsx
+++ src/screens/AboutScreen.jsx
@@ -22,24 +22,24 @@ const AboutScreen = () => {
             <div className="container" style={{zIndex: 1, flexDirection:"column"}}>
                 <h1 style={{color: unitStyles[unit].textColor}}>About</h1>
                 <div className="about-section" style={{display: "flex", justifyContent:"space-between", alignItems:"top", paddingBottom:"50px"}}>
-                    <div style={{width: "70%", display:"flex", flexDirection:"row", alignItems:"center"}}>
+                    <div className="holder-card-container" style={{display:"flex", flexDirection:"row", alignItems:"center"}}>
                         <HolderCard label={"The Class"}>
                             <p>This 1-credit class (offered at Stanford in fall '22) addresses intersections of writing systems and tech. Stanford has stellar NLP, yet I noticed a coursework gap. To fill it, I proposed this student-initiated class to the faculty senate. (And built this website over winter break!)</p>
                             <p>Ten wonderful students enrolled, ranging from undeclared freshmen to Computer Science upperclassmen to master's students from East Asian studies. The small class size fostered great questions, conversations, and in-class team work.</p>
                         </HolderCard>
                     </div>
-                    <div style={{width: "30%", display: "flex", flexDirection: "column", justifyContent:"center", alignItems:"center", paddingLeft:"50px"}}>
+                    <div className='about-img about-img-1'>
                         <img src={stanfordlogo} alt="" style={{display: "block", width:"auto", maxHeight:"250px"}}/>
                     </div>
                 </div>
                 <div className="about-section" style={{display: "flex", justifyContent:"space-between"}}>
-                    <div style={{width: "30%", display: "flex", flexDirection: "column", justifyContent:"center", alignItems:"center", paddingRight:"50px"}}>
+                    <div className='about-img about-img-2'>
                         <img src={me} alt="" style={{display: "block", width:"auto", maxHeight:"250px"}}/>
                     </div>
-                    <div style={{width: "70%", display:"flex", flexDirection:"row", alignItems:"center"}}>
+                    <div className="holder-card-container" style={{display:"flex", flexDirection:"row", alignItems:"center"}}>
                     <HolderCard label={"German"}>
                         <p>Hi! I'm German Enik. At the moment of writing this, I'm  a 23-year-old Master's student at Stanford pursuing CS.</p>
-                        <p>I'm originally from Moscow. Growing up writing Cyrillic and speaking Russian fueled my interests in everything at the intersection of tech and language, which I also explored during my undergrad at Stanford studying CS (NLP) and Linguistics.</p>  
+                        <p>I'm originally from Moscow, so I grew up writing Cyrillic and speaking Russian. Immigrating to the U.S. fueled my interests in everything at the intersection of tech and language, which I also explored during my undergrad at Stanford studying CS (NLP) and Linguistics.</p>  
                         <p>Feel free to <a className="reach-out-link" style={{color:unitStyles[unit].textColor}} href="https://www.linkedin.com/in/germanenik/">reach out</a>!</p>                           
                     </HolderCard>
                     </div>
