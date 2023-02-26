49260f13cebae82fce5123910a6d413666df2419
German Enik <germanenik@gmail.com>
2023-02-26 14:38:02 -0800

    switching to hash router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import SplashScreen from './screens/SplashScreen';
 import UnitScreen from './screens/UnitScreen';
 import AboutScreen from './screens/AboutScreen';
